976b2261592b673a36c8b1d57d482c580e5643d8
HP\Marwa Babelly <marwaababelly@gmail.com>
2023-11-22 13:10:37 +0300

    Add Project section

---
+++ src/components/Projects.js
@@ -0,0 +1,98 @@
+import React from "react";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import { Col, Container, Row, Nav, Tab } from "react-bootstrap";
+import ProjectCard from "./ProjectCard";
+
+const Projects = () => {
+  const projects = [
+    {
+      title: "Business StartUp",
+      descriptions: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business StartUp",
+      descriptions: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business StartUp",
+      descriptions: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business StartUp",
+      descriptions: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business StartUp",
+      descriptions: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business StartUp",
+      descriptions: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.<br></br> Lorem Ipsum has been the
+                  industry's standard dummy text.
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.<br></br> Lorem Ipsum has been the
+                  industry's standard dummy text.
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-img-right" src={colorSharp2} />
+    </section>
+  );
+};
+
+export default Projects;
